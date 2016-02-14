c34f6a5decf51695f13c9ad963e5fb072a86f407
Ted <taeywhang@gmail.com>
2016-02-13 17:10:56 -0800

    Use redis togo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  ENV["REDISTOGO_URL"] = 'redis://h:pbsanepg2otkl3brv0kjqmb16uh@ec2-54-83-32-60.compute-1.amazonaws.com:24089'
 end
--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,4 @@
 require 'redis'
 
-$redis = Redis.new(url: ENV['REDIS_URL'])
+uri = URI.parse(ENV["REDISTOGO_URL"])
+REDIS = Redis.new(:url => uri)
