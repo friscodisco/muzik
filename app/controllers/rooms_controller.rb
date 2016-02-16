8c4cce98de54b6554c40638ae27c4baaf976628a
Ted <taeywhang@gmail.com>
2016-02-15 20:36:01 -0800

    Added basic CRUD code for rooms

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,29 @@
 class RoomsController < ApplicationController
+  def index
+    @rooms = Room.all
+  end
+
+  def new
+    @new_room = Room.new
+  end
+
+  def create
+    @room = Room.new(room_params)
+
+    if @room.save
+      redirect_to @room
+    else
+      render json: { error: "Couldn't create room" }, status: 422
+    end
+  end
+
+  def show
+    @room = Room.find(params[:id])
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'root#index'
 
   resources 'users'
+  resources 'rooms'
 
   mount ActionCable.server => '/cable'
 end
