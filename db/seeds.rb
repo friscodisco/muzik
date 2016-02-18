195b8311c6539c63c28d65f0d4c9cf4866e4131f
Ted <taeywhang@gmail.com>
2016-02-17 22:45:21 -0800

    Create seeds file for development

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+def create_room(name, image_name)
+  Room.create!(name: name, image_name: image_name)
+end
+
+create_room("Classic", "classic.jpg")
+create_room("Piano", "piano.jpg")
+create_room("Trance", "trance.jpg")
